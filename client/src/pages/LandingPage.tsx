86ffa915a0ab360ccfe999485e706a3efaca577b
이하진 <hajin1015@naver.com>
2025-04-28 15:45:08 +0900

    [S12P31A707-42] feat: 랜딩페이지 ui

---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,20 @@
+import PuppyImg from "../assets/puppy.png";
+
+const LandingPage = () => {
+  return (
+    <div className="bg-bg h-full flex flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center">
+        <div>친구들과 꾸미는 공간</div>
+        <div className="text-title">미니피아</div>
+      </div>
+      <div className="w-30">
+        <img src={PuppyImg} alt="귀여운 강아지" />
+      </div>
+      <div className="w-[256px] h-[64px] flex justify-center items-center bg-red border-[2px] border-text rounded-xl cursor-pointer">
+        <div className="text-[40px] text-white tracking-[-0.011em]">START</div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
