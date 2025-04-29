067f28cc3cbdb293973122b5dc3e18e6270170d9
hajin1015@naver.com <hajin1015@naver.com>
2025-04-29 15:49:07 +0900

    [S12P31A707-51] chore:  소켓 연결에 필요한 라이브러리 설치

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis", // ✅ global 에러 해결
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
 });
