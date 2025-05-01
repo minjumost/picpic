de383bc0514d0d34c68476f4bb77ff08b4e13e5c
10hajin15@gmail.com <10hajin15@gmail.com>
2025-05-01 14:11:55 +0900

    [S12P31A707-56] fix: id 기반 remove로 수정

--- client/src/store/objectStore.ts
+++ client/src/store/objectStore.ts
@@ -1,38 +1,40 @@
-// src/store/objectStore.ts
 import { create } from "zustand";
 import { ServerObject, PlacedObject } from "../types/object";
 
 interface ObjectStore {
-  // 선택된 오브젝트 관리
   selectedObject: ServerObject | null;
   setSelectedObject: (obj: ServerObject | null) => void;
-
-  // 배치된 오브젝트 관리
   placedObjects: PlacedObject[];
   initPlacedObjects: (objects: PlacedObject[]) => void;
   addPlacedObject: (object: PlacedObject) => void;
-  removePlacedObject: (objectId: number, posX: number, posY: number) => void;
+  removePlacedObjectById: (roomObjectId: number) => void;
+  movePlacedObject: (
+    roomObjectId: number,
+    pos: { posX: number; posY: number }
+  ) => void;
   clearPlacedObjects: () => void;
 }
 
 export const useObjectStore = create<ObjectStore>((set) => ({
-  // 선택된 오브젝트 관리
   selectedObject: null,
-  initPlacedObjects: (objects) => set({ placedObjects: objects }),
   setSelectedObject: (obj) => set({ selectedObject: obj }),
 
-  // 배치된 오브젝트 관리
   placedObjects: [],
+  initPlacedObjects: (objects) => set({ placedObjects: objects }),
   addPlacedObject: (object) =>
     set((state) => ({
       placedObjects: [...state.placedObjects, object],
     })),
-  removePlacedObject: (objectId, posX, posY) =>
+  removePlacedObjectById: (roomObjectId) =>
     set((state) => ({
       placedObjects: state.placedObjects.filter(
-        (obj) =>
-          // id가 다르거나 OR 위치가 다른 오브젝트만 남김
-          obj.id !== objectId || obj.posX !== posX || obj.posY !== posY
+        (obj) => obj.id !== roomObjectId
+      ),
+    })),
+  movePlacedObject: (roomObjectId, pos) =>
+    set((state) => ({
+      placedObjects: state.placedObjects.map((obj) =>
+        obj.id === roomObjectId ? { ...obj, ...pos } : obj
       ),
     })),
   clearPlacedObjects: () => set({ placedObjects: [] }),
