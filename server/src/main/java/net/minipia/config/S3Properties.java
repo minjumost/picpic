a4e9dbd060664bcdbd1b269a2d43bf741cdfa2e1
seoyoung <seoyoung526@naver.com>
2025-05-02 15:43:25 +0900

    [S12P31A707-59]<feat>: s3에셋 올리기 + db에 저장

---
+++ server/src/main/java/net/minipia/config/S3Properties.java
@@ -0,0 +1,19 @@
+package net.minipia.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws.s3")
+@Getter
+@Setter
+public class S3Properties {
+	private String bucket;
+	private String region;
+	private String accessKey;
+	private String secretKey;
+}
+
