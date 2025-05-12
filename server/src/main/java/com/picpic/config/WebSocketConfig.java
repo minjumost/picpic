d43f8a138a604a411a83d04b60fb9f6bf8e750c4
Minju Choi <minjumost@naver.com>
2025-05-12 15:14:51 +0900

    [S12P31A707-99] chore: add origin

--- server/src/main/java/com/picpic/config/WebSocketConfig.java
+++ server/src/main/java/com/picpic/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/connection").setAllowedOrigins("localhost:5173").withSockJS();
+		registry.addEndpoint("/connection").setAllowedOrigins("*").withSockJS();
 	}
 
 	@Override
