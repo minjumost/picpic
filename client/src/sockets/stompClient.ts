325cad689f2a32f718e25f5dd006f704da0a7898
hajin1015@naver.com <hajin1015@naver.com>
2025-05-12 17:15:58 +0900

    [S12P31A707-105] fix: url 수정

--- client/src/sockets/stompClient.ts
+++ client/src/sockets/stompClient.ts
@@ -4,7 +4,7 @@ import SockJS from "sockjs-client";
 export let stompClient: Client | null = null;
 
 export const initStompClient = () => {
-  const socket = new SockJS(`${import.meta.env.VITE_BASE_URL}/wss`);
+  const socket = new SockJS(`${import.meta.env.VITE_BASE_URL}/connection`);
   stompClient = new Client({
     webSocketFactory: () => socket,
     debug: (str) => console.log("[STOMP]", str),
