ca31871a6e75f39325559d87ba47369d8f73eee8
seoyoung <seoyoung526@naver.com>
2025-05-14 09:49:57 +0900

    [S12P31A707-115] feat: 콜라주 사진 조회

--- server/src/main/java/com/picpic/service/CollageService.java
+++ server/src/main/java/com/picpic/service/CollageService.java
@@ -1,12 +1,18 @@
 package com.picpic.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.picpic.common.exception.ApiException;
 import com.picpic.common.exception.ErrorCode;
+import com.picpic.dto.collage.GetCollageListResponseDTO;
 import com.picpic.entity.Collage;
+import com.picpic.entity.Photo;
 import com.picpic.entity.Session;
 import com.picpic.repository.CollageRepository;
+import com.picpic.repository.PhotoRepository;
 import com.picpic.repository.SessionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +23,7 @@ public class CollageService {
 
 	private final CollageRepository collageRepository;
 	private final SessionRepository sessionRepository;
+	private final PhotoRepository photoRepository;
 
 	public void createCollage(Long sessionId, String collageImageUrl) {
 		Session session = sessionRepository.findBySessionId(sessionId).orElseThrow(
@@ -29,5 +36,20 @@ public class CollageService {
 
 		collageRepository.save(collage);
 
+	}
+
+	public List<GetCollageListResponseDTO> getCollages(Long sessionId) {
+		Session session = sessionRepository.findBySessionId(sessionId).orElseThrow(
+			() -> new ApiException(ErrorCode.NOT_FOUND_SESSION)
+		);
+
+		List<Photo> photos = photoRepository.findAllBySession(session);
+
+		return photos.stream()
+			.map(photo -> new GetCollageListResponseDTO(
+				photo.getSlotIndex(),
+				photo.getEditedImageUrl()
+			))
+			.collect(Collectors.toList());
 	}
 }
