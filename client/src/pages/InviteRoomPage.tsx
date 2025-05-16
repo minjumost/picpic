c768c6d88025e20b846b7cc65f09445e023522d4
Jun <kylekwon@naver.com>
2025-05-16 16:36:34 +0900

    [S12P31A707-144]: feat: 입장+대기방 합침침

--- client/src/pages/InviteRoomPage.tsx
+++ client/src/pages/InviteRoomPage.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router";
-import { useState } from "react";
 import { initStompSession } from "../sockets/stompClient";
 import { connectAndEnterSession } from "../sockets/sessionSocket";
 import { useSessionCode } from "../hooks/useSessionCode";
@@ -7,16 +6,6 @@ import { useSessionCode } from "../hooks/useSessionCode";
 const InviteRoomPage: React.FC = () => {
   const navigate = useNavigate();
   const sessionCode = useSessionCode();
-  const roomUrl = `https://localhost:5173/roomPwd?r=${sessionCode}`;
-
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(roomUrl).then(() => {
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000); // 2초 후 복원
-    });
-  };
 
   const handleEnterRoom = async () => {
     if (!sessionCode) {
@@ -35,35 +24,16 @@ const InviteRoomPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center w-full h-full p-4 gap-8">
+    <div className="flex flex-col items-center justify-center w-full h-full p-4 gap-8">
       {/* 제목 */}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 items-center">
         <h2 className="text-[24px] font-bold text-gray-800">
           방 생성이 완료됐어요🥳
         </h2>
-        <p className="text-[18px] font-semibold text-gray-500">
-          링크를 복사해서 친구를 초대해보세요.
-        </p>
-      </div>
-
-      {/* 링크 박스 + 복사 버튼 */}
-      <div className="w-full flex items-center justify-between pl-3 p-0.5 rounded-lg border border-gray-300 bg-white text-gray-700">
-        <span className="truncate mr-2 text-xl py-3">{roomUrl}</span>
-        <button
-          onClick={handleCopy}
-          className={`w-24 h-full text-md font-semibold px-3 py-3 rounded-md transition-colors duration-200
-              ${
-                copied
-                  ? "bg-green-500 text-white"
-                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-              }`}
-        >
-          {copied ? "Copyed" : "Copy"}
-        </button>
       </div>
 
       <button
-        className="py-3 rounded-lg font-semibold text-lg w-full bg-main1 text-white px-6 cursor-pointer"
+        className="py-3 items-center rounded-lg font-semibold text-lg w-[350px] bg-main1 text-white px-6 cursor-pointer"
         onClick={handleEnterRoom}
       >
         입장하기
--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import 소개1 from "../../assets/소개1.png";
+import 소개2 from "../../assets/소개2.png";
+import 소개3 from "../../assets/소개3.png";
+import 소개4 from "../../assets/소개4.png";
 import LoginButtons from "./LoginButtons";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const LandingPage: React.FC = () => {
+  const images = [소개1, 소개2, 소개3, 소개4];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: "60px",
+  };
+
   return (
     <div className="h-full flex flex-col justify-center items-center text-center p-4 gap-8">
       {/* Logo */}
@@ -13,21 +34,24 @@ const LandingPage: React.FC = () => {
         </p>
       </div>
 
-      {/* 이미지 대체 */}
-      <div className="relative w-full h-full bg-red-50">
-        {/* <div className="absolute w-28 h-40 bg-white border-2 border-gray-500 shadow-lg transform -rotate-12 top-4 left-2">
-          <div className="grid grid-rows-2 h-full">
-            <div className="border-b border-gray-400"></div>
-            <div className=""></div>
-          </div>
-        </div>
-        <div className="absolute w-28 h-40 bg-white border-2 border-gray-500 shadow-lg transform rotate-6 top-8 right-2">
-          <div className="grid grid-rows-3 h-full">
-            <div className="border-b border-gray-400"></div>
-            <div className="border-b border-gray-400"></div>
-            <div className=""></div>
-          </div>
-        </div> */}
+      {/* Carousel */}
+      <div className="w-[400px] max-w-xl mx-auto h-[350px]">
+        <Slider {...settings}>
+          {images.map((image, idx) => (
+            <div
+              key={idx}
+              className="h-[350px] flex items-center justify-center"
+            >
+              <div className="w-full h-full rounded-2xl shadow-xl bg-white overflow-hidden flex items-center justify-center transition-all duration-300">
+                <img
+                  src={image}
+                  alt={`소개 ${idx + 1}`}
+                  className="w-full h-[350px] object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
 
       <LoginButtons />
